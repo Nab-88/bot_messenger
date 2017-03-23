2501fffdb6f2f0088c66dd5e93aa298db03c42bd
Marvin Lerousseau <marvin.lerousseau@gmail.com>
2017-03-23 15:42:57 +0100

    lkallala

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var homepage = require('./routes/index');
-var webhook = require('./routes/webhook');
-
 
 var app = express();
 
